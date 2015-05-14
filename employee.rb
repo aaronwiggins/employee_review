6d63b44ac3284d3ea095f6ccb7c1df1aba2a1069
Aaron Wiggins <apw7177@gmail.com>
2015-05-14 15:08:23 -0400

    fixed argument issue when creating a new Employee

--- employee.rb
+++ employee.rb
@@ -1,8 +1,8 @@
 class Employee
   attr_reader :name, :email, :phone_number, :salary
 
-    def initialize(name:, email: "blank@email.com",
-                  phone_number: "9191234567", salary: "50000")
+    def initialize(name: "", email: "",
+                  phone_number: "", salary: "")
       @name = name
       @email = email
       @phone_number = phone_number
--- employee_department_test.rb
+++ employee_department_test.rb
@@ -21,37 +21,25 @@ class EmployeeDepartmentTest < Minitest::Test
   end
 
   def test_create_employee_name
-    assert_equal "Bob", Employee.new(name: "Bob",
-                                    email: "Bob@email.com",
-                                    phone_number: "9192223333",
-                                    salary: "95000").name
+    assert_equal "Bob", Employee.new(name: "Bob").name
   end
 
   def test_create_employee_with_email
-    assert_equal "Bob@email.com", Employee.new(name: "Bob",
-                                    email: "Bob@email.com",
-                                    phone_number: "9192223333",
-                                    salary: "95000").email
+    assert_equal "Bob@email.com", Employee.new(email: "Bob@email.com").email
   end
 
   def test_create_employee_phone_number
-    assert_equal "9192223333", Employee.new(name: "Bob",
-                                    email: "Bob@email.com",
-                                    phone_number: "9192223333",
-                                    salary: "95000").phone_number
+    assert_equal "9192223333", Employee.new(phone_number: "9192223333").phone_number
 
   end
 
   def test_create_employee_salary
-    assert_equal "95000", Employee.new(name: "Bob",
-                                    email: "Bob@email.com",
-                                    phone_number: "9192223333",
-                                    salary: "95000").salary
+    assert_equal "95000", Employee.new(salary: "95000").salary
 
   end
 
   def test_department_can_be_given_an_employee
-    name = Employee.new(name: "Bob")                                  
+    name = Employee.new(name: "Bob")
     department = Department.new("Accounting")
     assert department.assign_employee(name)
   end
